40bc90a0ef246ffdb0d72b0267f1ec7c36e4c244
kinyaying <kinyaying@gmail.com>
2021-03-02 15:34:20 +0800

    update .gitignore

--- .umi/core/history.ts
+++ .umi/core/history.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { createBrowserHistory } from '/Users/kin/MyCode/tapableUtil/node_modules/@umijs/runtime';
+import { createHashHistory } from '/Users/kin/MyCode/tapableUtil/node_modules/@umijs/runtime';
 
 let options = {
   "basename": "/"
@@ -9,10 +9,10 @@ if ((<any>window).routerBase) {
 }
 
 // remove initial history because of ssr
-let history: any = process.env.__IS_SERVER ? null : createBrowserHistory(options);
+let history: any = process.env.__IS_SERVER ? null : createHashHistory(options);
 export const createHistory = (hotReload = false) => {
   if (!hotReload) {
-    history = createBrowserHistory(options);
+    history = createHashHistory(options);
   }
 
   return history;
--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 
 export default {
+  history: { type: 'hash' },
   chainWebpack: (memo) => {
     // 更多配置 https://github.com/Microsoft/monaco-editor-webpack-plugin#options
     memo.plugin('monaco-editor-webpack-plugin').use(MonacoWebpackPlugin, [
